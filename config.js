39b29445c37e1597af047b0192beced90bc87541
hollo2ce <hollo2ce@dukes.jmu.edu>
2025-04-22 15:29:07 -0400

    inserted first image into chapter

--- config.js
+++ config.js
@@ -55,7 +55,7 @@ var config = {
             alignment: 'right',
             hidden: false,
             title: 'Someplace Georgia',
-            image: './assets/alice_walker_childhood_home.jpg',
+            image: './assets/rockingham_county_courthouse (1).jpeg',
             description: 'Description of the second location.',
             location: {
                 center: [-83.5573651, 32.5573651], 
